7ef26b4bf46eb9b00271fdddec05dde043d9b695
Francis Rubio <francisrubio@protonmail.com>
2022-02-27 12:06:05 +0800

    Added speaking

---
+++ src/collections/talks/future-js.md
@@ -0,0 +1,15 @@
+---
+title: Looking at a future with JavaScript
+description: An introductory talk about the basics of JavaScript and its use in web development.
+date: 2020-10-05
+host:
+  - name: Developer Student Club AMA Computer College Caloocan
+    description: Developer Student Club AMA Computer College Caloocan is a community of students that focus on improving their skills as well as their professional and personal network in the industry with the use of technology.
+event: 
+  - name: "DSC AMACC Caloocan: Web Development Series"
+    description: Developer Student Club AMA Computer College Caloocan is a community of students that focus on improving their skills as well as their professional and personal network in the industry with the use of technology.
+audience:
+  - Students from various schools with beginner to intermediate understanding of JavaScript as a language.
+duration:
+  - PT1H
+---
--- src/config/filters.js
+++ src/config/filters.js
@@ -1,23 +1,54 @@
 module.exports = {
   markdown: function (value) {
-    let markdown = require("markdown-it")({
-      html: true,
-    });
-    return markdown.render(value);
+    let markdown = require('markdown-it')({
+      html: true
+    })
+    return markdown.render(value)
   },
   icon: function (value) {
-    return `<svg class="feather" aria-hidden="true"><use href="/assets/images/feather-sprite.svg#${value}" /></svg>`;
+    return `<svg class="feather" aria-hidden="true"><use href="/assets/images/feather-sprite.svg#${value}" /></svg>`
   },
   machineReadableDate: function (value) {
-    const year = value.getFullYear();
-    const month = value.getMonth() + 1;
-    const date = value.getDate();
+    const year = value.getFullYear()
+    const month = value.getMonth() + 1
+    const date = value.getDate()
     return `${year.toString().padStart(4, 0)}-${month
       .toString()
-      .padStart(2, 0)}-${date.toString().padStart(2, 0)}`;
+      .padStart(2, 0)}-${date.toString().padStart(2, 0)}`
   },
   humanReadableDate: function (value) {
-    let formatter = new Intl.DateTimeFormat("en-US", { dateStyle: "long" });
-    return formatter.format(value);
+    let formatter = new Intl.DateTimeFormat('en-US', { dateStyle: 'long' })
+    return formatter.format(value)
+  },
+  duration: function (value) {
+    console.log('[DURATION]', value)
+    const regex = {
+      hours: /[0-9]*[Hh]/g,
+      minutes: /[0-9]*[Mm]/g,
+      seconds: /[0-9]*[Ss]/g,
+      number: /[0-9]*/g
+    }
+
+    const h = value.match(regex.hours)
+    const m = value.match(regex.minutes)
+    const s = value.match(regex.seconds)
+
+    const hours = h != null ? parseInt(h[0].match(regex.number)) : 0
+    const minutes = m != null ? parseInt(m[0].match(regex.number)) : 0
+    const seconds = s != null ? parseInt(s[0].match(regex.number)) : 0
+
+    const pluralRules = new Intl.PluralRules('en-us')
+    const units = {
+      hours: pluralRules.select(hours) === 'one' ? 'hour' : 'hours',
+      minutes: pluralRules.select(minutes) === 'one' ? 'minute' : 'minutes',
+      seconds: pluralRules.select(seconds) === 'one' ? 'second' : 'seconds'
+    }
+
+    const formatter = new Intl.ListFormat('en', { style: 'long', type: 'conjunction' });
+    return formatter.format([
+      hours > 0 ? `${hours} ${units.hours}` : null,
+      minutes > 0 ? `${minutes} ${units.minutes}` : null,
+      seconds > 0 ? `${seconds} ${units.seconds}` : null,
+    ].filter(s => s != null))
   }
-};
+}
