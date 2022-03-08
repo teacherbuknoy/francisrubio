aedb90f9f19d566ca432d167575e7b9897c24753
Francis Rubio <francisrubio@protonmail.com>
2022-03-08 12:53:53 +0800

    Revamped navigation bar

--- src/assets/scripts/animations/navigation-bar.js
+++ src/assets/scripts/animations/navigation-bar.js
@@ -1,44 +1,16 @@
 import { $, $$ } from '../utilities/dom'
+const navbar = $('.navigation-bar')
+const main = $('[data-observer-trigger]')
+const observer = new IntersectionObserver(observationCallback, { threshold: 0 })
+observer.observe(main)
 
-const eventCallback = (list, classes) => e => {
-  /** @type {HTMLElement} */
-  const target = e.target
-  const parentSelector = '.navigation-link:is(a, button)'
-  const childSelector = '.navigation-link:is(a, button) *'
-
-  if (target.matches(parentSelector) || target.matches(childSelector)) {
-    const link = target.matches(parentSelector) ? target : target.closest(parentSelector)
-    const {
-      offsetLeft: left,
-      offsetTop: top,
-      clientHeight: height,
-      clientWidth: width
-    } = link
-
-    list.classList.add(classes)
-    list.style.setProperty('--left', `${left}px`)
-    list.style.setProperty('--top', `${top}px`)
-    list.style.setProperty('--width', `${width}px`)
-    list.style.setProperty('--height', `${height}px`)
-  }
+function observationCallback(entries) {
+  entries.forEach(entry => {
+    const { target } = entry
+    if (entry.isIntersecting) {
+      navbar.classList.remove('attached')
+    } else {
+      navbar.classList.add('attached')
+    }
+  })
 }
-
-$$('[data-navigation-links]').forEach(ul => {
-  /** @type {HTMLElement} */
-  const list = ul
-  list.addEventListener('mouseover', eventCallback(list, 'hover'))
-  list.addEventListener('focusin', eventCallback(list, 'focus'))
-
-  list.addEventListener('mouseout', e => {
-    list.classList.remove('hover')
-  })
-
-  list.addEventListener('mouseleave', e => {
-    list.classList.remove('hover')
-    eventCallback(list, 'focus')({ target: document.activeElement })
-  })
-
-  list.addEventListener('focusout', e => {
-    list.classList.remove('focus')
-  })
-})
