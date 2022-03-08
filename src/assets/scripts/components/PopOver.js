1ab172091fb61e97c632ebedd7f30541fad654e7
Francis Rubio <francisrubio@protonmail.com>
2022-03-08 20:09:53 +0800

    Added feature cards

--- src/assets/scripts/components/PopOver.js
+++ src/assets/scripts/components/PopOver.js
@@ -5,75 +5,104 @@ class PopOver {
    * specify the [id] of the HTMLElement that will be the popover
    * @param {HTMLButtonElement} trigger the button that triggers this popover
    */
-  constructor(trigger) {
-    this.trigger = trigger;
-    const popover = document.getElementById(trigger.dataset.popover);
+  constructor (trigger) {
+    this.trigger = trigger
+    let popover = document.getElementById(trigger.dataset.popover)
 
     if (popover == null) {
-      throw Error("Popover is null");
+      if (trigger.nextElementSibling != null) {
+        popover = trigger.nextElementSibling
+      } else throw Error('Popover is null')
     }
-    this.events = { show: [], hide: [] };
-    this.trigger.addEventListener("click", (e) => this.toggle());
-    this.popOver = popover;
-    this._isShown = false;
+    this.events = { show: [], hide: [] }
+    this.trigger.addEventListener('click', e => this.toggle())
+    this.popOver = popover
+    this._isShown = false
 
-    document.addEventListener("keydown", (event) => {
-      if (event.key === "Escape") this.hide();
-    });
+    if (trigger.dataset.disclosure == null) {
+      document.addEventListener('keydown', event => {
+        if (event.key === 'Escape') this.hide()
+      })
 
-    document.addEventListener("click", (e) => {
-      if (!e.target.matches("button[data-popover], button[data-popover] *")) {
-        this.hide();
-      }
-    });
+      document.addEventListener('click', e => {
+        if (!e.target.matches('button[data-popover], button[data-popover] *')) {
+          this.hide()
+        }
+      })
+    }
 
-    this.hide();
+    if (trigger.dataset.ariaHide != null) {
+      this.ariaHide = true
+    }
+
+    this.hide()
+  }
+
+  set ariaHide (value) {
+    if (value) {
+      this.trigger.dataset.ariaHide = true
+    } else {
+      this.trigger.removeAttribute('data-aria-hide')
+    }
+  }
+
+  get ariaHide () {
+    return this.trigger.dataset.ariaHide
   }
 
   /**
    * @
    */
-  set isShown(value) {
-    this._isShown = value;
-    this.toggle(value);
+  set isShown (value) {
+    this._isShown = value
+    this.toggle(value)
   }
 
-  get isShown() {
-    return this._isShown;
+  get isShown () {
+    return this._isShown
   }
 
   /**
    * Shows this popover
    */
-  show() {
-    this.trigger.setAttribute("aria-expanded", "false");
-    this.popOver.removeAttribute("hidden");
-    this._isShown = true;
+  show () {
+    this.trigger.parentElement.setAttribute('data-open', 'true')
+    this.trigger.setAttribute('aria-expanded', 'false')
+    this.popOver.removeAttribute('hidden')
+    this.popOver.removeAttribute('aria-hidden')
+    this._isShown = true
 
-    this.events.show.forEach((event) => event());
+    this.events.show.forEach(event => event())
   }
 
   /**
    * Hides this popover
    */
-  hide() {
-    this.trigger.setAttribute("aria-expanded", "false");
-    this.popOver.setAttribute("hidden", "true");
-    this._isShown = false;
+  hide () {
+    this.trigger.parentElement.removeAttribute('data-open')
+    this.trigger.setAttribute('aria-expanded', 'false')
 
-    this.events.hide.forEach((event) => event());
+    if (this.ariaHide) {
+      this.popOver.setAttribute('aria-hidden', 'true')
+    } else {
+      this.popOver.setAttribute('hidden', 'true')
+    }
+
+    this._isShown = false
+
+    this.events.hide.forEach(event => event())
   }
 
   /**
    * Toggles this popover
    * @param {boolean} isPopoverShown if null, toggles this popover; otherwise, true shows this popover, false hides it
    */
-  toggle(isPopoverShown) {
+  toggle (isPopoverShown) {
     const popoverIsShown =
-      isPopoverShown == null ? this.isShown : isPopoverShown;
+      isPopoverShown == null ? this.isShown : isPopoverShown
 
-    if (popoverIsShown) this.hide();
-    else this.show();
+    if (popoverIsShown) this.hide()
+    else this.show()
   }
 
   /**
@@ -81,8 +110,8 @@ class PopOver {
    * @param {String} type the type of event to listen for
    * @param {Function} fn the function to execute when the event is triggered
    */
-  on(type, fn) {
-    this.events[type].push(fn);
+  on (type, fn) {
+    this.events[type].push(fn)
   }
 
   /**
@@ -90,13 +119,13 @@ class PopOver {
    * @param {String} type the type of event to listen for
    * @param {Function} fn the function to execute when the event is triggered
    */
-  off(type, fn) {
-    const index = this.events[type].indexOf(fn);
-    if (index > -1) this.events[type].splice(index, 1);
+  off (type, fn) {
+    const index = this.events[type].indexOf(fn)
+    if (index > -1) this.events[type].splice(index, 1)
   }
 }
 
-const popoverTriggers = document.querySelectorAll("button[data-popover]");
-const popovers = [...popoverTriggers].map((button) => new PopOver(button));
+const popoverTriggers = document.querySelectorAll('button[data-popover]')
+const popovers = [...popoverTriggers].map(button => new PopOver(button))
 
 export { PopOver, popovers }
--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -1,6 +1,7 @@
 import './animations/navigation-bar'
 import './animations/button'
 import './components/Dialog'
+import './components/PopOver'
 
 window.addEventListener('load', e => {
   document.body.classList.remove('no-js')
