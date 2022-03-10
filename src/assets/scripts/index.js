5854444890dd4b8f3e38cbc38c69f508a33dd0dd
Francis Rubio <francisrubio@protonmail.com>
2022-03-10 22:17:18 +0800

    Bug fixes

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -8,23 +8,27 @@ window.addEventListener('load', e => {
   document.body.classList.remove('no-js')
 })
 
-let tabs = [...document.querySelectorAll('[data-tab]:is(button, a)')].map(tab => {
-  let panel = null
+let tabs = [...document.querySelectorAll('[data-tab]:is(button, a)')].map(
+  tab => {
+    let panel = null
 
-  if (tab.matches('a') && tab.href) {
-    let selector = new URL(tab.href).hash
-    panel = document.querySelector(selector)
-  } else if (tab.matches('button') && tab.dataset.tab) {
-    panel = document.getElementById(tab.dataset.tab)
-  } else {
-    throw Element(
-      'Tab should be an anchor element that points to an existing element in its href, ' +
-        'or a button with a data-tab that has an ID of a panel.'
-    )
+    if (tab.matches('a') && tab.href) {
+      let selector = new URL(tab.href).hash
+      panel = document.querySelector(selector)
+    } else if (tab.matches('button') && tab.dataset.tab) {
+      panel = document.getElementById(tab.dataset.tab)
+    } else {
+      throw Element(
+        'Tab should be an anchor element that points to an existing element in its href, ' +
+          'or a button with a data-tab that has an ID of a panel.'
+      )
+    }
+
+    return new Tab(tab, panel)
   }
+)
 
-  return new Tab(tab, panel)
-})
-
-const tabControl = new TabControl(tabs)
-tabControl.addEventListener('change', e => console.log('[CHANGE]', e))
+if (tabs != null && tabs.length > 0) {
+  const tabControl = new TabControl(tabs)
+  tabControl.addEventListener('change', e => console.log('[CHANGE]', e))
+}
