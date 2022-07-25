1649cddaf973c97b8a1c837d9cce622d2c5fb4ca
Francis Rubio <francis@antaresph.dev>
2022-07-25 16:23:17 +0800

    Added time to read component

--- src/config/plugins.js
+++ src/config/plugins.js
@@ -1,5 +1,25 @@
 module.exports = {
   syntaxHighlight: function () {
     return { plugin: require('@11ty/eleventy-plugin-syntaxhighlight') }
+  },
+  timeToRead: function () {
+    return {
+      plugin: require('eleventy-plugin-time-to-read'),
+      options: {
+        output: data => {
+          console.log("[TIME TO READ]", data)
+
+          const includeHours = data.hours != null
+          const includeMinutes = data.minutes != null && !includeHours
+          let hours = includeHours ? `${data.hours} hour` : ''
+          let minutes = includeMinutes ? `${data.minutes} minute` : ''
+          
+          const timing = [hours, minutes].filter(s => s.length > 0)
+          const listFormatter = new Intl.ListFormat('en', { style: 'long', type: 'conjunction' })
+
+          return `${listFormatter.format(timing)} read`
+        }
+      }
+    }
   }
 }
