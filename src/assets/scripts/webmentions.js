4808ebc1050fc88b52704c8fa90c72bde574733b
Francis Rubio <96061229+teacherbuknoy@users.noreply.github.com>
2022-11-07 20:56:33 +0800

    Add data to webmentions summary

--- src/assets/scripts/webmentions.js
+++ src/assets/scripts/webmentions.js
@@ -4,6 +4,12 @@ const wm = new WebMentions()
 wm.getMentionsCount()
   .then(data => {
     console.log("[WEBMENTIONS]", data)
+    const { type } = data
+
+    Object.keys(type).forEach(t => {
+      document.querySelectorAll(`[data-webmention=${t}]`)
+        .forEach(element => {
+          element.innerText = type[t]
+        })
+    })
   })
-
-
