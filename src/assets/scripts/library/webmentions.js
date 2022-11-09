366f50483337a6353295dcd1ee4d99e78d19d9d3
Francis Rubio <96061229+teacherbuknoy@users.noreply.github.com>
2022-11-09 22:44:48 +0800

    Refactor and document webmentions

--- src/assets/scripts/library/webmentions.js
+++ src/assets/scripts/library/webmentions.js
@@ -5,12 +5,13 @@ const domain = siteData.domains.find(i => i.isPreferred).value
 const base = `https://${domain}`
 const path = new URL(document.URL).pathname
 
+/* If on localhost, change hostname to remote for WebMentions param */
 const PAGE_URL = new URL(document.URL).hostname == 'localhost'
   ? new URL(path, base).toString()
   : document.URL
 
 /** 
- * IDs of templates to use
+ * IDs of HTML templates to use
  * @type {Object}
  */
 const TEMPLATES = Object.freeze({
@@ -28,7 +29,20 @@ const WebMentionType = Object.freeze({
   REPOST: 'repost-of'
 })
 
+/**
+ * @description The helper class for implementing WebMentions
+ * @author Francis Rubio
+ * @class WebMentions
+ * @property {object} data the data received from WebMentions endpoint
+ * @property {string} url the URL of the page that receives the likes, reposts, etc.
+ */
 class WebMentions {
+  /**
+   * Creates an instance of WebMentions.
+   * @author Francis Rubio
+   * @param {string} url the URL of the page that receives the likes, reposts, etc.
+   * @memberof WebMentions
+   */
   constructor(url) {
     this.url = url == null
       ? TESTING
@@ -37,17 +51,12 @@ class WebMentions {
       : url
   }
 
-  async fetch() {
-    const API = new URL('https://webmention.io/api/mentions.jf2')
-    const params = API.searchParams
-
-    params.append("target", this.url)
-
-    const data = await fetch(API.toString()).then(data => data.json())
-    this.data = data.children
-    return data
-  }
-
+  /**
+   * @description Gets the total count of WebMention responses received by this page
+   * @author Francis Rubio
+   * @returns {Promise<WMStatistics>}
+   * @memberof WebMentions
+   */
   async getMentionsCount() {
     const API = new URL('https://webmention.io/api/count')
     const params = API.searchParams
@@ -58,37 +67,119 @@ class WebMentions {
     return data
   }
 
+  /**
+   * @description Gets all responses that have content (i.e. mention-of, in-reply-to)
+   * @author Francis Rubio
+   * @returns {Promise<WMEntry[]>}
+   * @throws Will throw an error if the data property is null. Use WebMentionsBuilder to avoid.
+   * @memberof WebMentions
+   */
   async getResponses() {
-    const data = (await this.fetch()).children
+    if (this.data == null) {
+      throw new Error("WebMentions data is null. Did you use the WebMentionsBuilder class to create this object?")
+    }
+
     const monitoredKeys = ['mention-of', 'in-reply-to']
-    const responses = data.filter(i => monitoredKeys.includes(i['wm-property']))
+    const responses = this.data.filter(i => monitoredKeys.includes(i['wm-property']))
 
     return responses
   }
 
+  /**
+   * @description Gets all WebMentions responses
+   * @author Francis Rubio
+   * @returns {Promise<WMEntry[]>}
+   * @throws Will throw an error if the data property is null. Use WebMentionsBuilder to avoid.
+   * @memberof WebMentions
+   */
   async getAllMentions() {
-    const data = await this.fetch()
+    if (this.data == null) {
+      throw new Error("WebMentions data is null. Did you use the WebMentionsBuilder class to create this object?")
+    }
+
     return data.children
   }
 
+  /**
+   * @description Gets all `like-of` responses
+   * @author Francis Rubio
+   * @returns {Promise<WMEntry[]>}
+   * @throws Will throw an error if the data property is null. Use WebMentionsBuilder to avoid.
+   * @memberof WebMentions
+   */
   async getLikes() {
     if (this.data == null) {
-      await this.fetch()
+      throw new Error("WebMentions data is null. Did you use the WebMentionsBuilder class to create this object?")
     }
 
     return this.data.filter(i => i['wm-property'] === 'like-of')
   }
 
+  /**
+   * @description Gets all `repost-of` responses
+   * @author Francis Rubio
+   * @returns {Promise<WMEntry[]>}
+   * @throws Will throw an error if the data property is null. Use WebMentionsBuilder to avoid.
+   * @memberof WebMentions
+   */
   async getReposts() {
     if (this.data == null) {
-      await this.fetch()
+      throw new Error("WebMentions data is null. Did you use the WebMentionsBuilder class to create this object?")
     }
 
     return this.data.filter(i => i['wm-property'] === 'repost-of')
   }
+
+  /**
+   * @description Returns true if there are no WebMentions responses
+   * @author Francis Rubio
+   * @returns {boolean}
+   * @memberof WebMentions
+   */
+  isEmpty() {
+    return this.data.length <= 0
+  }
+}
+
+/**
+ * @description Initializes a WebMentions object
+ * @author Francis Rubio
+ * @returns {Promise<WMEntry[]>}  
+ */
+async function fetchWebMentions() {
+  const API = new URL('https://webmention.io/api/mentions.jf2')
+  const params = API.searchParams
+
+  params.append("target", this.url)
+
+  const data = await fetch(API.toString()).then(data => data.json())
+  this.data = data.children
+  return data
+}
+class WebMentionBuilder {
+  /**
+   * @description Creates a new WebMentions object and initializes its `data` property.
+   * @author Francis Rubio
+   * @static
+   * @param {string} url the URL of the page that receives the likes, reposts, etc. 
+   * @returns {WebMentions}  
+   * @memberof WebMentionBuilder
+   */
+  static async build(url) {
+    const wm = new WebMentions(url)
+    await fetchWebMentions.apply(wm)
+
+    return wm
+  }
 }
 
 class WebMentionResponse {
+  /**
+   * Creates an instance of WebMentionResponse.
+   * @author Francis Rubio
+   * @param {WMEntry} data
+   * @memberof WebMentionResponse
+   */
   constructor(data) {
     this.author = data.author
     this.content = data.content
@@ -98,6 +189,12 @@ class WebMentionResponse {
     this.timestamp = new Date(Date.parse(data['wm-received']))
   }
 
+  /**
+   * @description Creates an HTML element out of the WebMentions data
+   * @author Francis Rubio
+   * @returns {HTMLLIElement}  
+   * @memberof WebMentionResponse
+   */
   render() {
     switch (this.type) {
       case WebMentionType.LIKE:
@@ -107,13 +204,20 @@ class WebMentionResponse {
     }
   }
 
+  /**
+   * @description Creates an HTML element out of this object's data if
+   *  it is a `like-of` or `mentions-of` entry
+   * @author Francis Rubio
+   * @returns {HTMLLIElement}  
+   * @memberof WebMentionResponse
+   */
   #renderLike() {
     const template = document.getElementById(TEMPLATES.LIKE)
     const element = template.content.firstElementChild.cloneNode(true)
 
     element.setAttribute('id', `wmr-${this.id}`)
     element.setAttribute('data-webmention-type', this.type)
-    
+
     const photoLink = element.querySelector('a[data-webmention-entry=author-link]')
     photoLink.setAttribute('href', this.author.url)
 
@@ -139,5 +243,39 @@ class WebMentionResponse {
   }
 }
 
+/**
+ * The statistics of WebMentions responses received by a page
+ * @typedef {Object} WMStatistics 
+ * @property {number} count the total number of responses received
+ * @property {object} type the different types of responses received
+ * @property {number} type.like the number of "like-of" responses
+ * @property {number} type.mention the number of "mention-of" responses
+ * @property {number} type.repost the number of "repost-of" responses
+ * @property {number} type.reply the number of "in-reply-to" responses
+ */
 
-export { WebMentions, WebMentionResponse, WebMentionType }
+/**
+ * The data about WebMentions responses
+ * @typedef {Object} WMEntry
+ * @property {WMAuthor} author the author of this entry
+ * @property {string} url the url of the page that sent this response entry
+ * @property {string} published the timestamp this entry was published
+ * @property {WMContent} content the content of this entry
+ */
+
+/**
+ * The content of a WebMentions entry
+ * @typedef {Object} WMContent
+ * @property {string} text the content of this entry in plain text
+ * @property {string} html the content of this entry in HTML
+ */
+
+/**
+ * The data about a WebMentions entry author
+ * @typedef {Object} WMAuthor
+ * @property {string} name the name of the author
+ * @property {string} photo the URL of the author's photo
+ * @property {string} url the URL of the author's profile
+ */
+
+export { WebMentions, WebMentionResponse, WebMentionType, WebMentionBuilder }
--- src/assets/scripts/webmentions.js
+++ src/assets/scripts/webmentions.js
@@ -1,45 +1,60 @@
-import { WebMentionResponse, WebMentions } from './library/webmentions'
+import { WebMentionBuilder, WebMentionResponse, WebMentions } from './library/webmentions'
 
-const wm = new WebMentions()
-wm.getMentionsCount()
-  .then(data => {
-    const { type } = data
-    const monitoredKeys = ["like", "mention", "reply"]
-    const actualKeys = Object.keys(type).filter(i => monitoredKeys.includes(i))
 
-    if (actualKeys.length <= 0)
-      return
+async function renderWebMentions() {
+  const wm = await WebMentionBuilder.build()
 
-    actualKeys.forEach(t => {
-      document.querySelectorAll(`[data-webmention=${t}]`)
-        .forEach(element => {
-          element.innerText = type[t]
+  if (wm.isEmpty()) {
+    console.log("[WebMentions]", "No WebMentions response found.")
+    return
+  }
+
+  await wm.getMentionsCount()
+    .then(data => {
+      const { type } = data
+      const monitoredKeys = ["like", "mention", "reply"]
+      const actualKeys = Object.keys(type).filter(i => monitoredKeys.includes(i))
+
+      if (actualKeys.length <= 0)
+        return
+
+      actualKeys.forEach(t => {
+        document.querySelectorAll(`[data-webmention=${t}]`)
+          .forEach(element => {
+            element.innerText = type[t]
+          })
+      })
+    })
+
+  await wm.getLikes()
+    .then(data => {
+      const responses = data.map(d => new WebMentionResponse(d).render())
+
+      document.querySelectorAll('[data-webmention-container=likes]:is(ul, ol)')
+        .forEach(container => {
+          responses.forEach(element => {
+            const clone = element.cloneNode(true)
+            container.appendChild(clone)
+          })
         })
     })
-  })
 
-wm.getLikes()
-  .then(data => {
-    const responses = data.map(d => new WebMentionResponse(d).render())
+  await wm.getReposts()
+    .then(data => {
+      const responses = data.map(d => new WebMentionResponse(d).render())
 
-    document.querySelectorAll('[data-webmention-container=likes]:is(ul, ol)')
-      .forEach(container => {
-        responses.forEach(element => {
-          const clone = element.cloneNode(true)
-          container.appendChild(clone)
+      document.querySelectorAll('[data-webmention-container=likes]:is(ul, ol)')
+        .forEach(container => {
+          responses.forEach(element => {
+            const clone = element.cloneNode(true)
+            container.appendChild(clone)
+          })
         })
-      })
-  })
+    })
 
-wm.getReposts()
-  .then(data => {
-    const responses = data.map(d => new WebMentionResponse(d).render())
+  document.querySelectorAll('.hidden[data-webmention-container]')
+    .forEach(el => el.classList.remove('hidden'))
+}
 
-    document.querySelectorAll('[data-webmention-container=likes]:is(ul, ol)')
-      .forEach(container => {
-        responses.forEach(element => {
-          const clone = element.cloneNode(true)
-          container.appendChild(clone)
-        })
-      })
-  })
+
+renderWebMentions()
