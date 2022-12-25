1048e88f8d0e2f50dcb257aacb90e7312bf7107e
Francis Rubio <96061229+teacherbuknoy@users.noreply.github.com>
2022-12-26 00:44:34 +0800

    Add image and breadcrumb to posts

--- src/assets/scripts/animations/button.js
+++ src/assets/scripts/animations/button.js
@@ -1,5 +1,5 @@
 document.addEventListener('mousemove', e => {
-  if (e.target.matches('.button:is(a, button)')) {
+  if (e.target.matches('.button:is(a, button), [data-animation=button]:is(a, button)')) {
     const button = e.target
     const { offsetX, offsetY } = e
     button.style.setProperty('--hover-top', `${offsetY}px`)
