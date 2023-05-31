f60c443a1e25aeda3dd21f9ff9e6838272ecaf95
Francis Rubio <96061229+teacherbuknoy@users.noreply.github.com>
2023-06-01 01:30:39 +0800

    Add analytics lambda

--- lambda/analytics.js
+++ lambda/analytics.js
@@ -1,23 +1,30 @@
 const { GoogleSpreadsheet } = require('google-spreadsheet')
-require('dotenv').config
+require('dotenv').config()
 
 exports.handler = async (event, context, callback) => {
   try {
     const doc = new GoogleSpreadsheet('170OW7wdMrchhjngyEqhzzZQR5s3EWdrpS9NAMC_rD_E')
-    await doc.useServiceAccountAuth({ client_email: process.env.GCP_CLIENT_EMAIL, private_key: process.env.GCP_PRIVATE_KEY })
+    await doc.useServiceAccountAuth({
+      client_email: process.env.GCP_CLIENT_EMAIL,
+      private_key: process.env.GCP_PRIVATE_KEY
+    })
     await doc.loadInfo()
+    const sheet = doc.sheetsByTitle['francisrub.io']
 
     const data = JSON.parse(event.body)
-    const addedRow = await StyleSheet.addRow(data)
+    console.log(data)
+    const rows = await sheet.addRows(data)
 
+    console.log(rows)
     return {
       statusCode: 200,
       body: JSON.stringify({ message: 'Data recorded.' })
     }
   }
   catch (e) {
+    console.log(e.toString())
     return {
-      statusCode: 500,
+      statusCode: e.response.status,
       body: e.toString()
     }
   }
