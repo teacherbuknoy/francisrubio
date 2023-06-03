bc8c708367d970cb590dffe88f4b707e8c688187
Francis Rubio <96061229+teacherbuknoy@users.noreply.github.com>
2023-06-04 04:11:00 +0800

    Add a way to disable analytics

--- src/assets/scripts/analytics.js
+++ src/assets/scripts/analytics.js
@@ -1,41 +1,45 @@
 const { installAnalytics, AnalyticsEvents } = require('./library/analytics')
 
-const analytics = installAnalytics('/.netlify/functions/analytics')
+const analyticsIsEnabled = localStorage.getItem('analytics') !== 'disabled'
 
-window.addEventListener('DOMContentLoaded', e => analytics.logEvent(AnalyticsEvents.SITE_VISIT))
-window.addEventListener('beforeunload', e => analytics.logEvent(AnalyticsEvents.SITE_LEAVE))
+if (analyticsIsEnabled) {
+  const analytics = installAnalytics('/.netlify/functions/analytics')
 
-document.addEventListener('click', e => {
-  if (e.target.matches('a, a *')) {
-    let link = document.createElement('a')
-    link = e.target.closest('a')
+  window.addEventListener('DOMContentLoaded', e => analytics.logEvent(AnalyticsEvents.SITE_VISIT))
+  window.addEventListener('beforeunload', e => analytics.logEvent(AnalyticsEvents.SITE_LEAVE))
 
-    const url = link.href
+  document.addEventListener('click', e => {
+    if (e.target.matches('a, a *')) {
+      let link = document.createElement('a')
+      link = e.target.closest('a')
 
-    if (url.startsWith('mailto')) {
-      analytics.logEvent(AnalyticsEvents.NAVIGATE, 'Email', url)
+      const url = link.href
+
+      if (url.startsWith('mailto')) {
+        analytics.logEvent(AnalyticsEvents.NAVIGATE, 'Email', url)
+      }
+      else if (url.startsWith('tel')) {
+        analytics.logEvent(AnalyticsEvents.NAVIGATE, 'Phone', url)
+      }
+      else if (link.hasAttribute('download')) {
+        analytics.logEvent(AnalyticsEvents.DOWNLOAD, 'URL', url)
+      }
+      else {
+        analytics.logEvent(AnalyticsEvents.NAVIGATE, 'URL', url)
+      }
+
     }
-    else if (url.startsWith('tel')) {
-      analytics.logEvent(AnalyticsEvents.NAVIGATE, 'Phone', url)
-    }
-    else if (link.hasAttribute('download')) {
-      analytics.logEvent(AnalyticsEvents.DOWNLOAD, 'URL', url)
-    }
-    else {
-      analytics.logEvent(AnalyticsEvents.NAVIGATE, 'URL', url)
+
+    if (e.target.matches('[data-dialog=modal__email], [data-dialog=modal__email] *')) {
+      analytics.logEvent(AnalyticsEvents.NAVIGATE, 'Form', location.toString() + '#modal__email')
     }
 
-  }
+    if (e.target.matches('[data-dialog=search-menu], [data-dialog=search-menu] *')) {
+      analytics.logEvent(AnalyticsEvents.NAVIGATE, 'Search', location.toString() + '#search-menu')
+    }
+  })
 
-  if (e.target.matches('[data-dialog=modal__email], [data-dialog=modal__email] *')) {
-    analytics.logEvent(AnalyticsEvents.NAVIGATE, 'Form', location.toString() + '#modal__email')
-  }
-
-  if (e.target.matches('[data-dialog=search-menu], [data-dialog=search-menu] *')) {
-    analytics.logEvent(AnalyticsEvents.NAVIGATE, 'Search', location.toString() + '#search-menu')
-  }
-})
-
-document.addEventListener('colorschemechange', e => {
-  analytics.logEvent(AnalyticsEvents.CONFIG_CHANGE, 'Color Scheme', e.detail.colorScheme)
-})
+  document.addEventListener('colorschemechange', e => {
+    analytics.logEvent(AnalyticsEvents.CONFIG_CHANGE, 'Color Scheme', e.detail.colorScheme)
+  })
+}
