1c6205e2fca83b06c7b3cf2e92a57887f72958c6
Francis Rubio <96061229+teacherbuknoy@users.noreply.github.com>
2023-06-19 17:43:49 +0800

    Add save button on color scheme changers

--- src/assets/scripts/components/ColorScheme.js
+++ src/assets/scripts/components/ColorScheme.js
@@ -1,17 +1,21 @@
 window.COLOR_SCHEME_KEY = 'color-scheme'
 
-/** @type {HTMLFormElement} */
-const colorSchemeSelector = document.forms['color-scheme']
-colorSchemeSelector.addEventListener('change', e => {
-  const selectedTheme = colorSchemeSelector['color-scheme'].value
-  applyColorScheme(selectedTheme)
-  localStorage.setItem(COLOR_SCHEME_KEY, selectedTheme)
-})
-
 // Apply preferred theme from localStorage
 const preferredColorScheme = localStorage.getItem(COLOR_SCHEME_KEY)
 applyColorScheme(preferredColorScheme)
-colorSchemeSelector['color-scheme'].value = preferredColorScheme
+
+/** @type {HTMLFormElement} */
+const colorSchemeSelectors = document.querySelectorAll('form[data-color-scheme]')
+colorSchemeSelectors.forEach(form => {
+  form.addEventListener('submit', e => {
+    e.preventDefault()
+    const selectedTheme = form['color-scheme'].value
+    applyColorScheme(selectedTheme)
+    localStorage.setItem(COLOR_SCHEME_KEY, selectedTheme)
+  })
+
+  form['color-scheme'].value = preferredColorScheme
+})
 
 function applyColorScheme(colorScheme) {
   document.body.classList.remove('light', 'dark', 'system')
