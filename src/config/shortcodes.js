e479dfdf8a334181fde2cdaaa51c3132ea7aa730
Francis Rubio <96061229+teacherbuknoy@users.noreply.github.com>
2023-06-21 12:15:13 +0800

    Restore missing shortcodes

--- src/config/shortcodes.js
+++ src/config/shortcodes.js
@@ -1,5 +1,6 @@
 
 
 module.exports = {
-  
+  padStart: (string, length, filler) => (string + '').padStart(length, filler),
+  padEnd: (string, length, filler) => (string + '').padEnd(length, filler),
 }
