03b9d38857f887a95d86fd8dfd8ca69874e5f99d
Francis Rubio <96061229+teacherbuknoy@users.noreply.github.com>
2023-09-13 18:16:04 +0800

    Remove social.antaresph.dev

--- src/data/socials.js
+++ src/data/socials.js
@@ -5,15 +5,15 @@ const mastodon = {
   accountIds: [
     '109319595958763014',
     '109810743922547925',
-    '110673367912097624'
+    //'110673367912097624'
   ],
   endpoints: {
-    profile: 'https://social.antaresph.dev/api/v1/accounts/110673367912097624',
-    //profile: 'https://masto.ai/api/v1/accounts/109810743922547925',
+    //profile: 'https://social.antaresph.dev/api/v1/accounts/110673367912097624',
+    profile: 'https://masto.ai/api/v1/accounts/109810743922547925',
     feed: 'https://masto.ai/api/v1/accounts/109810743922547925/statuses/?limit=40',
     feeds: [
       'https://masto.ai/api/v1/accounts/109810743922547925/statuses/?limit=40',
-      'https://social.antaresph.dev/api/v1/accounts/110673367912097624/statuses?limit=40'
+      //'https://social.antaresph.dev/api/v1/accounts/110673367912097624/statuses?limit=40'
     ]
   }
 }
