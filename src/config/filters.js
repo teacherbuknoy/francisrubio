8b66eb989401e66f61368cd5b2cde5d6a7177268
Francis Rubio <96061229+teacherbuknoy@users.noreply.github.com>
2024-02-07 15:39:41 +0800

    Migrate feathericons to filter

--- src/config/filters.js
+++ src/config/filters.js
@@ -141,5 +141,6 @@ module.exports = {
     const nodes = dom.querySelectorAll('body > *')
     return [...nodes].length
   },
-  featured: collection => collection.find(item => item.data.featured)
+  featured: collection => collection.find(item => item.data.featured),
+  log: str => console.log('[TEMPLATE]', str)
 }
