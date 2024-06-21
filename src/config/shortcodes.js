7b9bf006ef663ee4820a06a2a90d7411f0e11d2c
Francis Rubio <96061229+teacherbuknoy@users.noreply.github.com>
2024-06-22 04:45:51 +0800

    Refactor gallery layout

--- src/config/shortcodes.js
+++ src/config/shortcodes.js
@@ -24,5 +24,20 @@ module.exports = {
   markdown: {
     isPaired: true,
     shortcode: (content) => md.render(content)
+  },
+  gallery: {
+    isPaired: true,
+    shortcode: (filename, style, alt, width = 720, height = 1280) => `
+    <figure class="gallery-card" style="${style}">
+      <a class="gallery-card__link no-arrow" href="${site.api.images}${filename}" target="_blank">
+        <picture>
+          <source type="image/avif" srcset="${site.api.images}/tr:w-720,f-avif/${filename}">
+          <source type="image/webp" srcset="${site.api.images}/tr:w-720,f-webp/${filename}">
+          <source type="image/jpg" srcset="${site.api.images}/tr:w-720,f-jpg/${filename}">
+          <img src="${site.api.images}/tr:w-720,f-jpg/${filename}" alt="${alt}" width="${width}" height="${height}">
+        </picture>
+      </a>
+    </figure>
+    `
   }
 }
