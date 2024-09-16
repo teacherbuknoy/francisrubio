02c4777c6847cb3040504aa7fb88695f7d6ba18c
Francis Rubio <96061229+teacherbuknoy@users.noreply.github.com>
2024-09-16 19:31:20 +0800

    Add new responsive-picture web component

---
+++ src/assets/scripts/webcomponents/ResponsivePicture.js
@@ -0,0 +1,110 @@
+class ResponsivePicture extends HTMLElement {
+  static observedAttributes = ["alt", "provider", "src", "sizes", "formats", "height", "width"]
+
+  constructor() {
+    super()
+
+    this.provider = "https://ik.imagekit.io/8jjzxcl9p"
+    this.sizes = [540, 720, 1080]
+    this.formats = ['avif', 'webp', 'jpg']
+    this.height = 0
+    this.width = 0
+    this.alt = ''
+    this.src = ''
+  }
+
+  connectedCallback() {
+    this.render()
+  }
+
+  attributeChangedCallback(name, oldValue, newValue) {
+    console.log('[ATTR_CHANGED]', { name, oldValue, newValue })
+    switch (name) {
+      case 'provider':
+        this.provider = newValue
+        break
+      case 'src':
+        this.src = newValue
+        break
+      case 'sizes':
+        this.sizes = newValue.split(',')
+        break
+      case 'formats':
+        this.formats = newValue.split(',')
+        break
+      case 'width':
+        this.width = +newValue
+        break
+      case 'height':
+        this.height = +newValue
+        break
+      case 'alt':
+        this.alt = newValue
+        break
+    }
+
+    this.render()
+  }
+
+  render() {
+    this.innerHTML = ''
+    const picture = document.createElement('picture')
+
+    const sources = createSource(this)
+    sources.forEach(source => picture.appendChild(source))
+
+    const img = createDefaultImage(this)
+    picture.appendChild(img)
+
+    this.appendChild(picture)
+  }
+}
+
+customElements.define("responsive-picture", ResponsivePicture)
+
+/**
+ * @description Creates a default image for a ResponsivePicture
+ * @author Francis Rubio
+ * @param {ResponsivePicture} picture
+ * @returns {HTMLImageElement}
+ */
+function createDefaultImage(picture) {
+  const img = document.createElement('img')
+  const filename = picture.getAttribute("src")
+  const provider = picture.provider
+  const alt = picture.alt
+
+  img.src = `${provider}${filename}`
+  img.alt = alt
+  img.width = picture.width
+  img.height = picture.height
+  console.log(img)
+
+  return img
+}
+
+/**
+ * @description Creates a <source> for a ResponsivePicture
+ * @author Francis Rubio
+ * @param {ResponsivePicture} picture
+ * @returns {HTMLSourceElement[]}
+ */
+function createSource(picture) {
+  const sizes = picture.sizes
+  const formats = picture.formats
+
+  const sizesAttr = sizes.map((size) => `(max-width: ${size}px) ${size}w`).join(',')
+  const source = formats.map(format => ({
+    srcset: sizes.map(size => `${picture.provider}/tr:f-${format},w-${size}${picture.src} ${size}w`),
+    format
+  }))
+    .map(({ srcset, format }) => {
+      const source = document.createElement('source')
+      source.srcset = srcset
+      source.sizes = sizesAttr
+      source.type = `image/${format}`
+      return source
+    })
+
+  return source
+}
